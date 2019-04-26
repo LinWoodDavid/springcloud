a446c8adda3b0d6c6cec72d77675eb43fff01279
LinWoodDavid <17610897521@163.com>
2019-04-26 18:43:22 +0800

    19.04.25 gateway

--- eureka-client/src/main/java/com/david/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/david/EurekaClientApplication.java
@@ -2,11 +2,9 @@ package com.david;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement        //启用事物
-@EnableEurekaClient
 @SpringBootApplication
 public class EurekaClientApplication {
 
